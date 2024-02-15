cb6bce989213dc42d83f821ba5a07a03930cfe36
sgarcy01 <sgarcy01@estudiantes.unileon.es>
2024-02-15 16:49:12 +0100

    holaMundo

--- holaMundo.c
+++ holaMundo.c
@@ -5,9 +5,11 @@ int main(){
     int nthreads, tid;
     #pragma omp parallel private(tid) //Create a group of threads
     {
+        //Each thread has a copy of the variable tid
         tid = omp_get_thread_num();
         nthreads = omp_get_num_threads();
         printf("Hello from thread %d of %d threads \n", tid, nthreads);
     }
+    //All threads ends and only exists the master thread (tid == 0)
     return 0;
 }
